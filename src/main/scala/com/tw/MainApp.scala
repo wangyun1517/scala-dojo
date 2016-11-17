20ce9960a69bda61b5aef1389d4c246759ac8be7
Yun Wang <yun.wang@rea-group.com>
2016-11-17 16:25:14 +0800

    finish first run app.

--- src/main/scala/com/tw/MainApp.scala
+++ src/main/scala/com/tw/MainApp.scala
@@ -2,10 +2,14 @@ package com.tw
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 
 import scala.concurrent.Future
+import scala.io.StdIn
 
 //import scala.concurrent.ExecutionContext.Implicits._
 object MainApp {
@@ -14,7 +18,16 @@ object MainApp {
     implicit val actorSystem = ActorSystem()
     implicit val materializer = ActorMaterializer()
     implicit val executeContext = actorSystem.dispatcher
-    val handler: (HttpRequest) => Future[HttpResponse] = ???
-    Http().bindAndHandleAsync(handler, "localhost", 8080)
+    val handler: (HttpRequest) => Future[HttpResponse] = Route.asyncHandler{
+      get{
+        complete("Hello world")
+      }
+    }
+    val server: Future[ServerBinding] = Http().bindAndHandleAsync(handler, "localhost", 8080)
+    println("start at localhost:8080")
+    StdIn.readLine()
+
+    server
+      .flatMap(_.unbind).onComplete(_ => actorSystem.terminate() )
   }
 }
